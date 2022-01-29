25c361fc8a4a14e4aed991d828e808c38c3c6fec
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-01-29 12:44:46 +0100

    edited commit email

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,5 @@ import Product from '../models/productModel.js';
 // @route    GET /api/products
 // @access   Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+ 
 });
