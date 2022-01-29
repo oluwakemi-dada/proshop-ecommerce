470ba2f73bb647f6f61c4e21649c4da28b218b15
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-01-29 16:02:49 +0100

    user login

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -26,5 +27,9 @@ const userSchema = mongoose.Schema(
   }
 );
 
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model('User', userSchema);
-export default User
+export default User;
