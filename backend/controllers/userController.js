cf27f092919ac8ce476925c941ae28e50acc4471
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-01-30 18:19:45 +0100

    Get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -28,9 +28,19 @@ const authUser = asyncHandler(async (req, res) => {
 // @route    GET /api/users/profile
 // @access   Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  // const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-  res.send('Success');
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export { authUser, getUserProfile };
