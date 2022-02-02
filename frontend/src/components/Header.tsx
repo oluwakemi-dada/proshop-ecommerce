3d70d5a7581ad80ef565cca0e8e2fa9df113d7af
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-02-02 14:34:39 +0100

    Fixed header position at the top

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -10,6 +10,10 @@ import { logout } from '../actions/userActions';
 import styles from '../styles/Header.module.scss';
 
 const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
