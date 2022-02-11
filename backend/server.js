b48108c09c979de81e6bce0326f413063e7b118c
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-02-11 21:00:41 +0100

    Order controller and route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 
 dotenv.config();
 connectDB();
@@ -16,6 +17,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
