3943cb0fa89b3f2dbbb1362075721fb46dcaa4b4
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-02-11 18:12:41 +0100

    Place order functionality

--- frontend/src/screens/PlaceOrderScreen.tsx
+++ frontend/src/screens/PlaceOrderScreen.tsx
@@ -9,7 +9,30 @@ import { ReduxState } from '../types';
 // import { savePaymentMethod } from '../actions/cartActions';
 
 const PlaceOrderScreen: FC = () => {
- 
+  const dispatch = useDispatch<AppDispatch>();
+
+  const cart = useSelector((state: ReduxState) => state.cart);
+
+  // CALCULATE PRICES
+
+  const addDecimals = (num: number) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+  };
+
+  const itemsPrice = Number(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  const shippingPrice = addDecimals(itemsPrice > 100 ? 0 : 100);
+  const taxPrice = addDecimals(Number(0.15 * itemsPrice));
+  const totalPrice = (
+    Number(itemsPrice) +
+    Number(taxPrice) +
+    Number(shippingPrice)
+  ).toFixed(2);
+
+  const placeOrderHandler = () => {
+    console.log('ORDER');
+  };
 
   return (
     <div className={styles.wrapper}>
@@ -19,35 +42,43 @@ const PlaceOrderScreen: FC = () => {
           <div className={styles.shipping}>
             <h2>SHIPPING</h2>
             <div>
-              <div>Shipping:</div>
-              <div> Futa, south gate., Akure 12345, Nigeria</div>
+              <div>Address:</div>
+              <div>
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}{' '}
+                {cart.shippingAddress.postalCode},{' '}
+                {cart.shippingAddress.country}.
+              </div>
             </div>
           </div>
           <div className={styles.paymentMethod}>
             <h2>PAYMENT METHOD</h2>
             <div>
               <div>Method:</div>
-              <div> PayPal</div>
+              <div>{cart.paymentMethod}</div>
             </div>
           </div>
           <div className={styles.orderItems}>
             <h2>ORDER ITEMS</h2>
             <div className={styles.itemsWrapper}>
-              {[1, 2, 3].map((item) => (
-                <div key={item} className={styles.orderItem}>
-                  <img
-                    src='/images/airpods.jpg'
-                    className={styles.orderImage}
-                    alt=''
-                  />
-                  <Link to={'!#'}>
-                    <div className={styles.orderLink}>
-                      Airpods Wireless Bluetooth Headphones
-                    </div>
-                  </Link>
-                  <div>{`${2} X $${49.99} = $${99.98}`}</div>
-                </div>
-              ))}
+              {cart.cartItems.length === 0 ? (
+                <Message msg='Your cart is empty' variant='default' />
+              ) : (
+                cart.cartItems.map((item, index) => (
+                  <div key={index} className={styles.orderItem}>
+                    <img
+                      src={item.image}
+                      className={styles.orderImage}
+                      alt={item.name}
+                    />
+                    <Link to={`/product/${item.product}`}>
+                      <div className={styles.orderLink}>{item.name}</div>
+                    </Link>
+                    <div>{`${item.qty} x $${item.price} = $${(
+                      item.qty * item.price
+                    ).toFixed(2)}`}</div>
+                  </div>
+                ))
+              )}
             </div>
           </div>
         </div>
@@ -55,22 +86,32 @@ const PlaceOrderScreen: FC = () => {
           <h2>ORDER SUMMARY</h2>
           <div className={styles.priceSummary}>
             <div>Items:</div>
-            <div>$99.98</div>
+            <div>${addDecimals(itemsPrice)}</div>
           </div>
           <div className={styles.priceSummary}>
             <div>Shipping:</div>
-            <div>$99.98</div>
+            <div>${shippingPrice}</div>
           </div>
           <div className={styles.priceSummary}>
             <div>Tax:</div>
-            <div>$99.98</div>
+            <div>${taxPrice}</div>
           </div>
           <div className={styles.priceSummary}>
             <div>Total:</div>
-            <div>$99.98</div>
+            <div>${totalPrice}</div>
           </div>
           <div className={styles.placeOrderBtnWrapper}>
-            <button>PLACE ORDER</button>
+            <button
+              className={
+                cart.cartItems.length !== 0
+                  ? styles.placeOrderBtnEnabled
+                  : styles.placeOrderBtnDisabled
+              }
+              disabled={cart.cartItems.length === 0}
+              onClick={placeOrderHandler}
+            >
+              PLACE ORDER
+            </button>
           </div>
         </div>
       </div>
