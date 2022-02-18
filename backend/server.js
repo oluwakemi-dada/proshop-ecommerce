4112583ee06837baeae5684227be87a5c0b1f81f
oluwakemi-dada <dadaoluwakemi97@gmail.com>
2022-02-18 14:14:49 +0100

    morgan setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
@@ -11,24 +12,33 @@ import uploadRoutes from './routes/uploadRoutes.js';
 dotenv.config();
 connectDB();
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// ROUTES
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
+// RETURN PAYPAL CLIENT ID
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
+// IMAGE UPLOADS
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+//ERROR HANDLERS
 app.use(notFound);
 app.use(errorHandler);
 
